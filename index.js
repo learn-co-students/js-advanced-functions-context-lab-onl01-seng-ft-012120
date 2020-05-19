325136ecd89201fb381d0068c2abdbd40fb702ce
xdundun <xdundun@gmail.com>
2020-05-19 12:05:51 -0400

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,69 @@
  for you to use if you need it!
  */
 
+let createEmployeeRecord = function(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(employeeArray) {
+    return employeeArray.map(function(employee){
+        return createEmployeeRecord(employee)
+    })
+}
+
+let createTimeInEvent = function(dateStamp) {
+    let[date,hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour,10),
+        date,
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp) {
+    let[date,hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour,10),
+        date,
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(dates) {
+    let hoursIn = this.timeInEvents.find(function(date) {
+        return date.date === dates
+    })
+
+    let hoursOut = this.timeOutEvents.find(function(date) {
+        return date.date === dates
+    })
+
+    return (hoursOut.hour - hoursIn.hour) / 100
+}
+
+let wagesEarnedOnDate = function(amountOfDays) {
+    let wages = hoursWorkedOnDate.call(this, amountOfDays)
+        * this.payPerHour
+    return wages
+}
+
+let calculatePayroll = function(employees) {
+    return employees.reduce(function(employee, checks){
+        return employee + allWagesFor.call(checks)
+    },0)
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +82,10 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+let findEmployeeByFirstName = function(employees,name) {
+    return employees.find(function(n) {
+        return n.firstName === name
+    })
 }
